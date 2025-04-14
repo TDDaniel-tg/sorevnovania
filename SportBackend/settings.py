fda73cb024801b7722d0ec89883bf90497d22a1f
TDDaniel-tg <veacheslavoo9@gmail.com>
2025-04-14 20:47:35 +0600

    first commit

--- SportBackend/settings.py
+++ SportBackend/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'main/static'),
+    os.path.join(BASE_DIR, 'SportBackend/main/static'),
 ]
 
 MEDIA_URL = '/media/'
