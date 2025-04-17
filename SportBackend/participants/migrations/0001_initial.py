2a00af84a239b0d323fe0e2b41a01c464343b5c6
TDDaniel-tg <veacheslavoo9@gmail.com>
2025-04-18 01:55:32 +0600

    first commit

--- SportBackend/participants/migrations/0001_initial.py
+++ SportBackend/participants/migrations/0001_initial.py
@@ -16,12 +16,12 @@ class Migration(migrations.Migration):
             name='Participant',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100, validators=[django.core.validators.RegexValidator(message='Имя должно содержать только буквы, пробелы и дефис', regex='^[А-Яа-яЁё\\s-]+$')])),
+                
                 ('phone_number', models.CharField(max_length=13, validators=[django.core.validators.RegexValidator(message='Номер телефона должен быть в формате +996XXXXXXXXX', regex='^\\+996[0-9]{9}$')])),
                 ('city', models.CharField(max_length=50, validators=[django.core.validators.RegexValidator(message='Город должен содержать только буквы, пробелы и дефис', regex='^[А-Яа-яЁё\\s-]+$')])),
                 ('weight_category', models.IntegerField(validators=[django.core.validators.MinValueValidator(30, message='Весовая категория не может быть меньше 30 кг'), django.core.validators.MaxValueValidator(150, message='Весовая категория не может быть больше 150 кг')])),
                 ('age', models.IntegerField(validators=[django.core.validators.MinValueValidator(6, message='Возраст не может быть меньше 6 лет'), django.core.validators.MaxValueValidator(100, message='Возраст не может быть больше 100 лет')])),
-                ('discipline', models.CharField(max_length=50, validators=[django.core.validators.RegexValidator(message='Дисциплина должна содержать только буквы, пробелы и дефис', regex='^[А-Яа-яЁё\\s-]+$')])),
+                
                 ('registration_date', models.DateTimeField(auto_now_add=True)),
             ],
             options={
